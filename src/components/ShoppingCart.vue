119597059ea168621c9611b5cb913e6fae0057f7
Luis <luisrubin55@gmail.com>
2023-12-29 20:30:13 -0600

    Proyecto POS

---
+++ src/components/ShoppingCart.vue
@@ -0,0 +1,54 @@
+<script setup>
+import ShoppingCartItem from './ShoppingCartItem.vue';
+import Amount from './Amount.vue';
+import CouponForm from './CouponForm.vue';
+import { useCartStore } from '@/stores/cart';
+import { useCouponStore } from '@/stores/coupons';
+import { formatCurrency } from '@/helpers';
+
+const cart = useCartStore()
+const coupon = useCouponStore()
+
+
+
+</script>
+
+<template>
+    <p v-if="cart.isEmpty" class="text-xl text-center text-gray-900">El Carrito esta vacio</p>
+
+    <div v-else>
+        <p class="text-xl font-bold text-gray-900 ">Resumen de Venta</p>
+        <ul
+            role="list"
+            class="mt-6 divide-y divide-gray-200"
+        >
+            <ShoppingCartItem
+                v-for="item in cart.items"
+                :key="item.id"
+                :item="item"
+            />
+        </ul>
+        <dl class="space-y-4 border-gray-200 pt-6 text-sm font-medium text-gray-500">
+            <Amount>
+                <template #label>Subtotal: </template>
+                {{ formatCurrency(cart.subtotal) }}
+            </Amount>
+            <Amount>
+                <template #label>Impuestos: </template>
+                {{ formatCurrency(cart.taxes) }}
+            </Amount>
+            <Amount>
+                <template #label>Total a pagar: </template>
+                {{ formatCurrency(cart.total) }}
+            </Amount>
+        </dl>
+        <CouponForm/>
+        <button
+            type="button"
+            class="mt-10 w-full bg-indigo-700 text-white uppercase p-3"
+            @click="cart.checkout"
+        >
+            Confirmar Compra
+        </button>
+    </div>
+</template>
---
+++ src/views/admin/NewProductView.vue
@@ -0,0 +1,113 @@
+<script setup>
+import { reactive } from 'vue'
+import { useRouter } from 'vue-router'
+import Link from '@/components/Link.vue';
+import useImage from '@/composables/useImage'
+import { useProductsStore } from '@/stores/products'
+
+
+const {url, onFileChange, isImageUploaded } = useImage()
+const products = useProductsStore()
+const router = useRouter()
+
+const formData = reactive({
+    name: '',
+    category: '',
+    price: '',
+    availability: '',
+    image: ''
+})
+
+const submitHandler = async data =>{
+    const { image, ...values } = data
+    try {
+        await products.createProduct({
+            ...values,
+            image: url.value
+        })
+        router.push({name: 'products'})
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+</script>
+
+<template>
+    <div>
+        <Link to="products">Volver</Link>
+        <h1 class="text-4xl font-black my-10">Nuevo Producto</h1>
+
+        <div class="flex justify-center bg-white shadow">
+            <div class="mt-10 p-10 w-full lg:w-2/4">
+
+                <FormKit 
+                    type="form"
+                    submit-label="Agregar Producto"
+                    incomplete-messages="No se pudo enviar, revisa los mensajes"
+                    @submit="submitHandler"
+                    :value="formData"
+                >
+
+                    <FormKit 
+                        type="text" 
+                        label="Nombre"
+                        name="name"
+                        placeholder="Nombre del producto"
+                        validation="required"
+                        :validation-messages="{required: 'El nombre del producto es obligatorio'}"
+                        v-model.trim="formData.name"
+                    />
+                    <FormKit 
+                        type="file" 
+                        label="Imagen Producto"
+                        name="image"
+                        validation="required"
+                        :validation-messages="{required: 'La imagen del producto es obligatoria'}"
+                        accept=".jpg"
+                        @change = "onFileChange"
+                        v-model.trim="formData.image"
+                    />
+                    <div v-if="isImageUploaded">
+                        <p class="font-black ">Imagen Producto: </p>
+                        <img :src="url" alt="Nueva imagen producto" class="w-32">
+                    </div>
+
+                    <FormKit 
+                        type="select" 
+                        label="Categoria"
+                        name="category"
+                        validation="required"
+                        :validation-messages="{required: 'La categoría es obligatoria'}"
+                        :options="products.categoryOpcions"
+                        v-model.number="formData.category"
+                    />
+                    <FormKit 
+                        type="number" 
+                        label="Precio"
+                        name="price"
+                        placeholder="Precio del producto"
+                        validation="required"
+                        :validation-messages="{required: 'El precio es obligatorio'}"
+                        min="1"
+                        v-model.number="formData.price"
+                    />
+                    <FormKit 
+                        type="number" 
+                        label="Disponibles"
+                        name="availability"
+                        placeholder="Cantidad Disponible"
+                        validation="required"
+                        :validation-messages="{required: 'La cantidad es obligatoria'}"
+                        min="1"
+                        v-model.number="formData.availability"
+                    />
+
+                </FormKit>
+            </div>
+
+        </div>
+
+    </div>
+</template>
+
